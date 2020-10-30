e2eb908f3637844a4378b5a5493503bbab0a8cb7
phanijsp <phani.jsp@gmail.com>
2020-10-30 17:39:25 +0530

    Fixed row index number of ResultSet absolute

--- src/main/java/QueryExecutor.java
+++ src/main/java/QueryExecutor.java
@@ -23,7 +23,7 @@ public class QueryExecutor {
         }
         if(resultSet!=null){
             try {
-                resultSet.absolute(0);
+                resultSet.absolute(1);
                 return Long.parseLong(resultSet.getString(2));
             } catch (SQLException throwables) {
                 throwables.printStackTrace();
